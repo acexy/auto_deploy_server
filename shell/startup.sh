99a3a6d4bde0eaea0737886d66469036a9362303
Acexy <thankjava@foxmail.com>
2017-06-13 15:07:42 +0800

    调整脚本

--- shell/startup.sh
+++ shell/startup.sh
@@ -27,12 +27,12 @@ cd $DIR
 DIR=`pwd`
 
 # 以得到的完整路径为ps搜索条件判断是否能ps到相关进程信息
-ps -ef | grep $DIR | grep -v grep > /dev/null 2>&1
-if [ $? -ne 0 ]; then # 是否ps到相关信息
+PID=`ps -ef | grep $DIR | grep -v grep | grep -v bash`
+if [ "$PID" != "" ]; then # 是否ps到相关信息
     # 执行启动命令 自定义
     STARTCMD
     sleep $SLEEPTIME
-    PID=`ps -ef | grep $DIR | grep -v grep | awk '{print $2}'`
+    PID=`ps -ef | grep $DIR | grep -v grep | grep -v bash | grep -v $0 | awk '{print $2}'`
     if [ "$PID" = "" ]; then
         # 启动进程后无法ps到进程号,启动失败
         echo_red "执行启动命令后未能ps到启动进程号!"
